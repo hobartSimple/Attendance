7ff0369d7d1d40bd11bd6ee0ed4e3531cb6408fc
HobartSimple <hobartbhq@outlook.com>
2017-07-28 19:28:59 +0800

    2017年7月28日19:27:30 调整目录结构 2017年7月28日19:27:57 添加基础数据

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './home/component/home.component';
 
 @NgModule({
   declarations: [
