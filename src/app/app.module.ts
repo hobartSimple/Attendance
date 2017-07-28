f2b529419a75ff8450a01771577034206caff3cf
HobartSimple <hobartbhq@outlook.com>
2017-07-28 15:21:13 +0800

    2017年7月28日10:15:12  添加@ng-bootstrap 2017年7月28日15:11:17  添加APP主路由 app-routing 2017年7月28日15:17:06  添加home页 home 2017年7月28日15:19:33  添加公共alert提示 shared - toast

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
