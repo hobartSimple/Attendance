f59ac51309d0cc07a3fdb6b221b32a8d9233ce04
HobartSimple <hobartbhq@outlook.com>
2017-08-09 20:49:21 +0800

    2017年8月9日20:48:27 优化一，

--- src/app/home/component/home.component.ts
+++ src/app/home/component/home.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
-//import { HomeService } from './../service/home.service';
+import { HomeService } from './../service/home.service';
 import { ToastComponent } from './../../shared/toast/toast.component';
 
 @Component({
@@ -15,7 +15,7 @@ export class HomeComponent implements OnInit {
   isLoading = true;
 
   constructor(private http: Http,
-             // private homeService: HomeService,
+              private homeService: HomeService,
               public toast: ToastComponent,
               private formBuilder: FormBuilder) { }
 
